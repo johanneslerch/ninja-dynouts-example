92113255ce1f8224e15627f5783897225cd2d0f7
Johannes Lerch <johannes.lerch@continental-corporation.com>
2024-08-14 10:17:15 +0200

    additional test case with and without conflicts + fixed ninja version

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 rem SET NINJA_EXE=ninja-1.11.1.conti.2.exe
-SET NINJA_EXE=ninja-pr2366-Jun12.exe
+rem SET NINJA_EXE=ninja-pr2366-Jun12.exe
+rem set NINJA_EXE=D:\git\ninja\ninja.exe
+SET NINJA_EXE=ninja-1.11.1.conti.3.exe
 
 DEL output.dynamic
 DEL output.static
@@ -13,6 +15,20 @@ DEL .ninja_log
 @echo invoke again, should be no work to do
 %NINJA_EXE% -d explain -f build1.ninja
 
+
 @echo ----------------------------------------
-@echo invoke with a changed build graph, should be no work to do
+@echo invoke with a changed build graph contining a conflict: should complain about conflict
 %NINJA_EXE% -d explain -f build2.ninja
+
+@echo ----------------------------------------
+@echo invoke again, should still complain about the conflict
+%NINJA_EXE% -d explain -f build2.ninja
+
+
+@echo ----------------------------------------
+@echo invoke with a changed build graph (no conflict anymore), should re-execute build statement
+%NINJA_EXE% -d explain -f build3.ninja
+
+@echo ----------------------------------------
+@echo invoke again, should be no work to do
+%NINJA_EXE% -d explain -f build3.ninja
